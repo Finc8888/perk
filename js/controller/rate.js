915e0c266158805f2271ecee5da6611fb7c79973
Finc8888 <con8@mail.ru>
2019-03-09 23:22:08 +0300

    Improve styles

--- js/controller/rate.js
+++ js/controller/rate.js
@@ -4,8 +4,11 @@ app.controller("rate",function($scope,$location){
 	$scope.pes = 0;
 	$scope.project_name = 'Текущий проект';
 	//$scope.baseUrl = $location.abcUrl();
+	$scope.round2 =function(val){
+		return parseFloat(val.toFixed(2));
+	}
 	$scope.mid_rate = function(){
-		return ($scope.opt+4*$scope.norm+$scope.pes)/6};
+		return $scope.round2(($scope.opt+4*$scope.norm+$scope.pes)/6)};
 	$scope.dif_rate = function(){
-		return ($scope.pes-$scope.opt)/6};
+		return $scope.round2(($scope.pes-$scope.opt)/6)};
 })
