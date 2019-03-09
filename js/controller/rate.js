63af1642de5537b4d09b6d0ea1c23d6aabeec274
Finc8888 <con8@mail.ru>
2019-03-09 21:59:19 +0300

    Adding style to header

--- js/controller/rate.js
+++ js/controller/rate.js
@@ -2,6 +2,7 @@ app.controller("rate",function($scope,$location){
 	$scope.opt = 0;
 	$scope.norm = 0;
 	$scope.pes = 0;
+	$scope.project_name = 'Текущий проект';
 	//$scope.baseUrl = $location.abcUrl();
 	$scope.mid_rate = function(){
 		return ($scope.opt+4*$scope.norm+$scope.pes)/6};
